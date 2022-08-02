Staging-ObelusFamily/Anythink-Market-awh84

feat: update docker solution readme (#55)

.wilco-helpers/scripts/update_child_branches.js
@@ -1,0 +1,194 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const args = require("yargs").argv;
+const axios = require("axios");
+
+const ENGINE_URL = 'https://wilco-engine.herokuapp.com';
+
+let git;
+let BRANCH;
+let ANYTHINK_REPO_PATH;
+let RESULTS = [];
+let BRANCHES_TO_UPDATE = [];
+let HAS_ERRORS = false;
+const branchHierarchyMap = {};
+const questHierarchyMap = {};
+
+function printUsage() {
+  console.log(
+    "Run using the command: \n node update_child_branches.js --repoPath=<local-path-to-market-base-repo> --branch=<branch-name> --onlyPrintTree"
+  );
+}
+
+async function main() {
+  if (!args.repoPath || !args.branch) {
+    printUsage();
+    return;
+  } else {
+    BRANCH = args.branch;
+    ANYTHINK_REPO_PATH = args.repoPath;
+  }
+
+  if (!process.env.API_SERVER_TOKEN) {
+    console.log("Please include the API_SERVER_TOKEN env var");
+  }
+
+  const { data } = await axios.get(`${ENGINE_URL}/api/v1/quests`, {
+    headers: { Authorization: `ServerToken ${process.env.API_SERVER_TOKEN}` }
+  });
+  const questsMap = data.quests.reduce((map, quest) => {
+    map[quest._id] = quest;
+    return map;
+  }, {});
+  branchHierarchyMap.main = ['quest_solution/tutorial'];
+  for (const quest of data.quests) {
+    if (quest.questDependency) {
+      const questDependency = questsMap[quest.questDependency];
+      if (quest.solutionBranch) {
+        branchHierarchyMap[questDependency.solutionBranch] = branchHierarchyMap[questDependency.solutionBranch] || [];
+        branchHierarchyMap[questDependency.solutionBranch].push(quest.solutionBranch);
+      }
+      questHierarchyMap[questDependency._id] = questHierarchyMap[questDependency._id] || [];
+      questHierarchyMap[questDependency._id].push(quest._id);
+    }
+  }
+  git = initGit(args.repoPath);
+  await git("status");
+
+  console.log("\n+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log("Tree of child branches for '" + args.branch + "':");
+  console.log("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
+  printTreeView(args.branch, branchHierarchyMap);
+
+  console.log("\n+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log("Tree of quests:");
+  console.log("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
+  printTreeView('tutorial', questHierarchyMap);
+
+  if (args.onlyPrintTree) {
+    return;
+  }
+
+  await updateChildBranches(args.branch);
+  if (args.forcePush === "true" && !HAS_ERRORS) {
+    await forcePushChildBranches();
+  }
+}
+
+async function forcePushChildBranches() {
+  console.log("\n+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log(
+    "Force pushing updated branches:\n",
+    "*",
+    BRANCHES_TO_UPDATE.join("\n * ")
+  );
+  console.log("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
+
+  await git(`push --force origin ${BRANCHES_TO_UPDATE.join(" ")}`);
+}
+
+const initGit = (path) => async (args) => {
+  return await exec(`git ${args}`, { cwd: path });
+};
+
+let treeOutput = "";
+
+function buildTreeView(item, hierarchyMap, tabs = 2) {
+  if (hierarchyMap[item]) {
+    hierarchyMap[item].map((childItem) => {
+      for (let i = 0; i < tabs; i++) {
+        treeOutput += " ";
+      }
+      treeOutput += `|_ ${childItem} \n`;
+      buildTreeView(childItem, hierarchyMap, tabs + 3);
+    });
+  }
+}
+
+function printTreeView(root, hierarchyMap) {
+  treeOutput = "";
+  console.log(root);
+  buildTreeView(root, hierarchyMap);
+  console.log(treeOutput);
+}
+
+async function rebaseTree(branch, branchHierarchyMap) {
+  if (branchHierarchyMap[branch]) {
+    for (const childBranch of branchHierarchyMap[branch]) {
+      const report = { branch: childBranch };
+      process.stdout.write(`* Rebasing "${childBranch}" onto "${branch}"...`);
+      try {
+        await git(`checkout ${childBranch}`)
+        await git(`rebase ${branch}`);
+        console.log(` DONE.`);
+        await rebaseTree(childBranch, branchHierarchyMap);
+      } catch (e) {
+        HAS_ERRORS = true;
+        report.error = e;
+        console.log(` ERROR.`);
+        if (e.stderr?.includes("git rebase --abort")) {
+          const { stdout } = await git("diff");
+          report.diff = stdout;
+          await git(`rebase --abort`);
+        }
+      } finally {
+        RESULTS.push(report);
+        BRANCHES_TO_UPDATE.push(childBranch);
+      }
+    }
+  }
+}
+
+async function updateChildBranches(branch) {
+  console.log("\n+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log("Updating all child branches of '" + branch + "':");
+  console.log("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
+  await git(`checkout ${branch}`);
+
+  await rebaseTree(branch, branchHierarchyMap);
+  await git(`checkout ${branch}`);
+
+  if (RESULTS.filter((b) => b.error).length > 0) {
+    console.log("\n DETAILED REPORT: \n");
+    RESULTS.forEach((branch) => {
+      console.log(
+        "--------------------------------------------------------------------------------"
+      );
+      console.log(
+        `##### "${branch.branch}": ${branch.error ? "FAILED" : "SUCCESS"}`
+      );
+
+      if (branch.error) {
+        console.log("\n# ERROR: \n" + branch.error.message);
+        if (branch.diff) {
+          console.log("# DIFF: \n" + branch.diff);
+        }
+      }
+    });
+    process.exit(1);
+  } else {
+    console.log("\n **** DONE SUCCESSFULLY.");
+  }
+}
+
+function getSubstringAfter(str, substr) {
+  return str.substring(str.indexOf(substr) + substr.length);
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((err) => {
+    if (err.code === "ENOENT") {
+      console.log(`ERROR: Path not found: ${ANYTHINK_REPO_PATH}`);
+    } else if (err?.stderr?.includes("you need to resolve")) {
+      console.log("ERROR: Git is in the middle of a conflict");
+    } else if (err?.stderr?.includes("did not match any file")) {
+      const badBranch = getSubstringAfter(err.cmd, "git checkout ");
+      console.log(`ERROR: Branch name "${badBranch}" not found`);
+    } else {
+      console.log(err);
+    }
+    process.exit(1);
+  });

backend/node/app.js
@@ -1,0 +1,104 @@
+require("dotenv").config();
+var http = require("http"),
+  path = require("path"),
+  methods = require("methods"),
+  express = require("express"),
+  bodyParser = require("body-parser"),
+  session = require("express-session"),
+  cors = require("cors"),
+  passport = require("passport"),
+  errorhandler = require("errorhandler"),
+  mongoose = require("mongoose");
+
+var isProduction = process.env.NODE_ENV === "production";
+
+// Create global app object
+var app = express();
+
+app.use(cors());
+
+// Normal express config defaults
+app.use(require("morgan")("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require("method-override")());
+app.use(express.static(__dirname + "/public"));
+
+app.use(
+  session({
+    secret: "conduit",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+if (!process.env.MONGODB_URI) {
+  console.warn("Missing MONGODB_URI in env, please add it to your .env file");
+}
+
+mongoose.connect(process.env.MONGODB_URI);
+if (isProduction) {
+} else {
+  mongoose.set("debug", true);
+}
+
+require("./models/User");
+require("./models/Item");
+require("./models/Comment");
+require("./config/passport");
+
+app.use(require("./routes"));
+
+/// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  if (req.url === "/favicon.ico") {
+    res.writeHead(200, { "Content-Type": "image/x-icon" });
+    res.end();
+  } else {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  }
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
+// finally, let's start our server...
+var server = app.listen(process.env.PORT || 3000, function() {
+  console.log("Listening on port " + server.address().port);
+});

backend/rails/db/schema.rb
@@ -1,0 +1,110 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_12_061614) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", id: :serial, force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_favorites_on_item_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "follows", id: :serial, force: :cascade do |t|
+    t.string "followable_type", null: false
+    t.integer "followable_id", null: false
+    t.string "follower_type", null: false
+    t.integer "follower_id", null: false
+    t.boolean "blocked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followable_id", "followable_type"], name: "fk_followables"
+    t.index ["followable_type", "followable_id"], name: "index_follows_on_followable_type_and_followable_id"
+    t.index ["follower_id", "follower_type"], name: "fk_follows"
+    t.index ["follower_type", "follower_id"], name: "index_follows_on_follower_type_and_follower_id"
+  end
+
+  create_table "items", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.string "slug"
+    t.string "description"
+    t.string "image"
+    t.integer "favorites_count"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_items_on_slug", unique: true
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "taggings", id: :serial, force: :cascade do |t|
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+    t.index ["taggable_type", "taggable_id"], name: "index_taggings_on_taggable_type_and_taggable_id"
+    t.index ["tagger_type", "tagger_id"], name: "index_taggings_on_tagger_type_and_tagger_id"
+  end
+
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "image"
+    t.text "bio"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+  
+  add_foreign_key "comments", "items"
+  add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "items"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "items", "users"
+
+end

Staging-ObelusFamily/Anythink-Market-bemsz

.wilco-helpers/scripts/update_child_branches.js
@@ -1,0 +1,194 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const args = require("yargs").argv;
+const axios = require("axios");
+
+const ENGINE_URL = 'https://engine.wilco.gg';
+
+let git;
+let BRANCH;
+let ANYTHINK_REPO_PATH;
+let RESULTS = [];
+let BRANCHES_TO_UPDATE = [];
+let HAS_ERRORS = false;
+const branchHierarchyMap = {};
+const questHierarchyMap = {};
+
+function printUsage() {
+  console.log(
+    "Run using the command: \n node update_child_branches.js --repoPath=<local-path-to-market-base-repo> --branch=<branch-name> --onlyPrintTree"
+  );
+}
+
+async function main() {
+  if (!args.repoPath || !args.branch) {
+    printUsage();
+    return;
+  } else {
+    BRANCH = args.branch;
+    ANYTHINK_REPO_PATH = args.repoPath;
+  }
+
+  if (!process.env.API_SERVER_TOKEN) {
+    console.log("Please include the API_SERVER_TOKEN env var");
+  }
+
+  const { data } = await axios.get(`${ENGINE_URL}/api/v1/quests`, {
+    headers: { Authorization: `ServerToken ${process.env.API_SERVER_TOKEN}` }
+  });
+  const questsMap = data.quests.reduce((map, quest) => {
+    map[quest._id] = quest;
+    return map;
+  }, {});
+  branchHierarchyMap.main = ['quest_solution/onboarding'];
+  for (const quest of data.quests) {
+    if (quest.questDependency) {
+      const questDependency = questsMap[quest.questDependency];
+      if (quest.solutionBranch) {
+        branchHierarchyMap[questDependency.solutionBranch] = branchHierarchyMap[questDependency.solutionBranch] || [];
+        branchHierarchyMap[questDependency.solutionBranch].push(quest.solutionBranch);
+      }
+      questHierarchyMap[questDependency._id] = questHierarchyMap[questDependency._id] || [];
+      questHierarchyMap[questDependency._id].push(quest._id);
+    }
+  }
+  git = initGit(args.repoPath);
+  await git("status");
+
+  console.log("\n+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log("Tree of child branches for '" + args.branch + "':");
+  console.log("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
+  printTreeView(args.branch, branchHierarchyMap);
+
+  console.log("\n+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log("Tree of quests:");
+  console.log("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
+  printTreeView('onboarding', questHierarchyMap);
+
+  if (args.onlyPrintTree) {
+    return;
+  }
+
+  await updateChildBranches(args.branch);
+  if (args.forcePush === "true" && !HAS_ERRORS) {
+    await forcePushChildBranches();
+  }
+}
+
+async function forcePushChildBranches() {
+  console.log("\n+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log(
+    "Force pushing updated branches:\n",
+    "*",
+    BRANCHES_TO_UPDATE.join("\n * ")
+  );
+  console.log("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
+
+  await git(`push --force origin ${BRANCHES_TO_UPDATE.join(" ")}`);
+}
+
+const initGit = (path) => async (args) => {
+  return await exec(`git ${args}`, { cwd: path });
+};
+
+let treeOutput = "";
+
+function buildTreeView(item, hierarchyMap, tabs = 2) {
+  if (hierarchyMap[item]) {
+    hierarchyMap[item].map((childItem) => {
+      for (let i = 0; i < tabs; i++) {
+        treeOutput += " ";
+      }
+      treeOutput += `|_ ${childItem} \n`;
+      buildTreeView(childItem, hierarchyMap, tabs + 3);
+    });
+  }
+}
+
+function printTreeView(root, hierarchyMap) {
+  treeOutput = "";
+  console.log(root);
+  buildTreeView(root, hierarchyMap);
+  console.log(treeOutput);
+}
+
+async function rebaseTree(branch, branchHierarchyMap) {
+  if (branchHierarchyMap[branch]) {
+    for (const childBranch of branchHierarchyMap[branch]) {
+      const report = { branch: childBranch };
+      process.stdout.write(`* Rebasing "${childBranch}" onto "${branch}"...`);
+      try {
+        await git(`checkout ${childBranch}`)
+        await git(`rebase ${branch}`);
+        console.log(` DONE.`);
+        await rebaseTree(childBranch, branchHierarchyMap);
+      } catch (e) {
+        HAS_ERRORS = true;
+        report.error = e;
+        console.log(` ERROR.`);
+        if (e.stderr?.includes("git rebase --abort")) {
+          const { stdout } = await git("diff");
+          report.diff = stdout;
+          await git(`rebase --abort`);
+        }
+      } finally {
+        RESULTS.push(report);
+        BRANCHES_TO_UPDATE.push(childBranch);
+      }
+    }
+  }
+}
+
+async function updateChildBranches(branch) {
+  console.log("\n+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log("Updating all child branches of '" + branch + "':");
+  console.log("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
+  await git(`checkout ${branch}`);
+
+  await rebaseTree(branch, branchHierarchyMap);
+  await git(`checkout ${branch}`);
+
+  if (RESULTS.filter((b) => b.error).length > 0) {
+    console.log("\n DETAILED REPORT: \n");
+    RESULTS.forEach((branch) => {
+      console.log(
+        "--------------------------------------------------------------------------------"
+      );
+      console.log(
+        `##### "${branch.branch}": ${branch.error ? "FAILED" : "SUCCESS"}`
+      );
+
+      if (branch.error) {
+        console.log("\n# ERROR: \n" + branch.error.message);
+        if (branch.diff) {
+          console.log("# DIFF: \n" + branch.diff);
+        }
+      }
+    });
+    process.exit(1);
+  } else {
+    console.log("\n **** DONE SUCCESSFULLY.");
+  }
+}
+
+function getSubstringAfter(str, substr) {
+  return str.substring(str.indexOf(substr) + substr.length);
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((err) => {
+    if (err.code === "ENOENT") {
+      console.log(`ERROR: Path not found: ${ANYTHINK_REPO_PATH}`);
+    } else if (err?.stderr?.includes("you need to resolve")) {
+      console.log("ERROR: Git is in the middle of a conflict");
+    } else if (err?.stderr?.includes("did not match any file")) {
+      const badBranch = getSubstringAfter(err.cmd, "git checkout ");
+      console.log(`ERROR: Branch name "${badBranch}" not found`);
+    } else {
+      console.log(err);
+    }
+    process.exit(1);
+  });

backend/node/app.js
@@ -1,0 +1,89 @@
+require("dotenv").config();
+var http = require("http"),
+  path = require("path"),
+  methods = require("methods"),
+  express = require("express"),
+  bodyParser = require("body-parser"),
+  session = require("express-session"),
+  cors = require("cors"),
+  passport = require("passport"),
+  errorhandler = require("errorhandler"),
+  mongoose = require("mongoose");
+
+var isProduction = process.env.NODE_ENV === "production";
+
+// Create global app object
+var app = express();
+
+app.use(cors());
+
+// Normal express config defaults
+app.use(require("morgan")("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require("method-override")());
+app.use(express.static(__dirname + "/public"));
+
+app.use(
+  session({
+    secret: "secret",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+if (!process.env.MONGODB_URI) {
+  console.warn("Missing MONGODB_URI in env, please add it to your .env file");
+}
+
+mongoose.connect(process.env.MONGODB_URI);
+if (isProduction) {
+} else {
+  mongoose.set("debug", true);
+}
+
+require("./models/User");
+require("./models/Item");
+require("./models/Comment");
+require("./config/passport");
+
+app.use(require("./routes"));
+
+/// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  if (req.url === "/favicon.ico") {
+    res.writeHead(200, { "Content-Type": "image/x-icon" });
+    res.end();
+  } else {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  }
+});
+
+/// error handler
+app.use(function(err, req, res, next) {
+  console.log(err.stack);
+  if (isProduction) {
+    res.sendStatus(err.status || 500)
+  } else {
+    res.status(err.status || 500);
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  }
+});
+
+// finally, let's start our server...
+var server = app.listen(process.env.PORT || 3000, function() {
+  console.log("Listening on port " + server.address().port);
+});

backend/rails/db/schema.rb
@@ -1,0 +1,110 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_12_061614) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", id: :serial, force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_favorites_on_item_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "follows", id: :serial, force: :cascade do |t|
+    t.string "followable_type", null: false
+    t.integer "followable_id", null: false
+    t.string "follower_type", null: false
+    t.integer "follower_id", null: false
+    t.boolean "blocked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followable_id", "followable_type"], name: "fk_followables"
+    t.index ["followable_type", "followable_id"], name: "index_follows_on_followable"
+    t.index ["follower_id", "follower_type"], name: "fk_follows"
+    t.index ["follower_type", "follower_id"], name: "index_follows_on_follower"
+  end
+
+  create_table "items", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.string "slug"
+    t.string "description"
+    t.string "image"
+    t.integer "favorites_count"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_items_on_slug", unique: true
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "taggings", id: :serial, force: :cascade do |t|
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+    t.index ["taggable_type", "taggable_id"], name: "index_taggings_on_taggable"
+    t.index ["tagger_type", "tagger_id"], name: "index_taggings_on_tagger"
+  end
+
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "image"
+    t.text "bio"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "comments", "items"
+  add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "items"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "items", "users"
+end